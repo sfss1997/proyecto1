f82a220b7366f9f0a76e2fc969071dee59f58533
sfss1997 <sergioss1997@gmail.com>
2020-05-02 16:24:23 -0600

    email

--- Proyecto/Proyecto/Controllers/HomeController.cs
+++ Proyecto/Proyecto/Controllers/HomeController.cs
@@ -12,5 +12,38 @@ namespace Proyecto.Controllers
         {
             return View();
         }
+       
+        public void SendEmail()
+        {
+            System.Net.Mail.MailMessage mmsg = new System.Net.Mail.MailMessage();
+            mmsg.To.Add("sergioss1997@gmail.com");
+            mmsg.Subject = "prueba";
+            mmsg.SubjectEncoding = System.Text.Encoding.UTF8;
+
+            mmsg.Body = "hola mundo";
+
+            mmsg.BodyEncoding = System.Text.Encoding.UTF8;
+            mmsg.IsBodyHtml = false;
+            mmsg.From = new System.Net.Mail.MailAddress("infoempresarialucr@gmail.com");
+
+            System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient();
+
+            client.Credentials = new System.Net.NetworkCredential("infoempresarialucr@gmail.com", "nws.2020");
+            client.Port = 587;
+            client.EnableSsl = true;
+            client.Host = "smtp.gmail.com";
+
+            try
+            {
+                client.Send(mmsg);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
     }
 }
---
+++ Proyecto/Proyecto/Scripts/home.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    emailTest();
+
+});
+
+function emailTest() {
+    $.ajax({
+        url: "/Home/SendEmail",
+        data: JSON.stringify(),
+        type: "POST",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+     
+
+        },
+        error: function (errorMessage) {
+            alert(errorMessage.responseText);
+        }
+    });
+
+}
