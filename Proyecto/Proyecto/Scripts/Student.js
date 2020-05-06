fe2e88ab3a2eefe052dae21b5b7f6461946b0157
Wilmata24 <wilmermata.24@gmail.com>
2020-05-06 16:06:58 -0600

    Javascript

--- Proyecto/Proyecto/Scripts/Student.js
+++ Proyecto/Proyecto/Scripts/Student.js
@@ -66,4 +66,61 @@ function loadLocation() {
 }
 
 
+function Add() {
+
+    var province = {
+        Id: $("#ProvinceDropdown option:selected").val()
+    };
+
+    var canton = {
+        Id: $("#CantonDropdown option:selected").val()
+    };
+
+    var district = {
+        Id: $("#DistrictDropdown option:selected").val()
+    };
+
+    var student = {
+
+        Id: $('#StudentId').val(),
+        StudentName: $('#Name').val(),
+        LastName: $('#LastName').val(),
+        Birthday: $('#Birthday').val(),
+        Mail: $('#Mail').val(),
+        Image: "En espera",
+        RegistrationStatus: "En espera"
+        
+       
+    };
+
+    var location = {
+        Province: province.Id,
+        Canton: canton.Id,
+        District: district.Id
+    };
+
+    var user = {
+
+        Id: $('#StudentId').val(),
+        Password: $('#Password').val(),
+        IsAdministrator: 0,
+        Status: "Activo"
+    };
+
+    $.ajax({
+        url: "/Home/Add",
+        data: JSON.stringify(student),
+        type: "POST",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            $('#myModal').modal('hide');
+        },
+        error: function (errorMessage) {
+            alert(errorMessage.responseText);
+        }
+    });
+}
+
+
 
