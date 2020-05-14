e6beb46634cf7c1fc1ab6898b971a72e79b2af7f
sfss1997 <sergioss1997@gmail.com>
2020-05-13 18:54:10 -0600

    User data y controller

---
+++ Proyecto/Proyecto/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Proyecto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        UserDataEF userDataEF = new UserDataEF();
+
+        public JsonResult VerifyTypeOfUser(int id)
+        {
+            return Json(userDataEF.VerifyTypeOfUser(id), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Loginr(String username, String password)
+        {
+            return Json(userDataEF.Login(username,password), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Proyecto/Proyecto/Models/UserDataEF.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto.Models
+{
+    public class UserDataEF
+    {
+
+        public String VerifyTypeOfUser(int id) {
+
+            using (var context = new Entities())
+            {
+                var user = context.SPTypeUserVerify(id).Single();
+
+                return user;
+
+            }
+
+        }
+
+        public String Login(String Username , String Password)
+        { 
+            using (var context = new Entities())
+            {
+                var user = context.SPUsersLogin(Username,Password).Single();
+
+                return user;
+
+            }
+    }
+}
