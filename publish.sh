60ad6ff2d9b3e62a6122e6e06affedf0be7125f2
sfss1997 <sergioss1997@gmail.com>
2020-05-20 23:54:01 -0600

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -5,7 +5,7 @@ set -e
 
 # https://gist.github.com/cjus/1047794
 echo 'Retrieving latest deploy...'
-url=`curl -H "Authorization: Bearer $NETLIFY_PUBLISH_KEY" https://api.netlify.com/api/v1/sites/proyectolenguajes/deploys`
+url=`curl -H "Authorization: Bearer $NETLIFY_PUBLISH_KEY" https://api.netlify.com/api/v1/sites/ucr.ac.cr/deploys`
 temp=`echo $url | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w -m 1 'id'`
 
 # https://www.netlify.com/docs/api/#deploys
